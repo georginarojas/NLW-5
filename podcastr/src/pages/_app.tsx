a346545b7aedbdd9ecdf498e1df58006a123792d
Georgina Rojas <ginarojas29@gmail.com>
2021-04-21 12:00:11 -0300

    Creating the Player component.

--- podcastr/src/pages/_app.tsx
+++ podcastr/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../components/Header";
+import { Player } from "../components/Player";
 
 import "../styles/global.scss";
 import styles from "../styles/app.module.scss";
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }) {
         <Header />
         <Component {...pageProps} />
       </main>
+      <Player />
     </div>
   );
 }

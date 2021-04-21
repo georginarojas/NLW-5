a9b4ee2ef44bfbe00936a5f7ca6f440465b4e968
Georgina Rojas <ginarojas29@gmail.com>
2021-04-21 11:58:10 -0300

    Creating the Header component.

---
+++ podcastr/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+
+import styles from "./styles.module.scss";
+
+export function Header() {
+  // https://date-fns.org/v2.21.1/docs/format
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBR,
+  });
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
+      <p>O melhor para você ouvir, sempre</p>
+      <span>{currentDate}</span>
+    </header>
+  );
+}
